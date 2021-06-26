58d111f122ba5c8280452d2661587aaf76313dbf
Sangsaka Wira <sangsakawira@gmail.com>
2021-06-26 22:59:38 +0700

    add middleware

--- routes/user.js
+++ routes/user.js
@@ -4,11 +4,11 @@ const router = express.Router()
 
 const userController = require("../controller/user")
 
-router.post("/",middleware.verifyAuth,userController.create)
+router.post("/",userController.create)
 router.post("/login",userController.login)
-router.get("/",middleware.verifyAuth,userController.read)
-router.get("/:id",middleware.verifyAuth,userController.readById)
-router.put("/",middleware.verifyAuth,userController.updateById)
-router.delete("/:id",middleware.verifyAuth,userController.delete)
+router.get("/",userController.read)
+router.get("/:id",userController.readById)
+router.put("/",userController.updateById)
+router.delete("/:id",userController.delete)
 
 module.exports = router
